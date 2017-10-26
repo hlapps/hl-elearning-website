e79416195a216a8dfdaf700923cbe576f44cd871
Andrés Madero Vergara <amaderov1787@gmail.com>
2017-10-25 20:57:56 -0500

    cambios_generales

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistryComponent } from './registry/registry.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { ContentComponent } from './content/content.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'signon', component: RegistryComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'curso', component: CursosComponent},
+    { path: 'content', component: ContentComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
